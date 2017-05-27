53b7e685ecf8966c4e3a6c87ec282cfe90089ce1
Hiroki Yasui <hy@192.168.2.111>
2017-05-27 16:09:52 +0200

    initial code

--- db/postgresql/postgresql.go
+++ db/postgresql/postgresql.go
@@ -1,1 +1,15 @@
 package postgresql
+
+import (
+	_ "github.com/lib/pq"
+	"database/sql"
+	"fmt"
+)
+
+func New(dbname, user, pass string){
+	db, err := sql.Open("postgres", fmt.Sprintf("dbname=%s user=%s password=%s sslmode=disable", dbname, user, pass))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
