ddde7fb29f987f9e197e9ed8934c49c164815d2a
Hiroki Yasui <hy@192.168.2.111>
2017-05-27 17:34:35 +0200

    commented out import mysql because it causes error on go1.8.3

--- db/mysql/mysql.go
+++ db/mysql/mysql.go
@@ -5,7 +5,9 @@ import (
 	"errors"
 	"fmt"
 	// mysql driver
-	_ "github.com/go-sql-driver/mysql"
+	// TODO:it may cause error on golang 1.8.3 or some version
+	// panic: sql: Register called twice for driver mysql
+	//_ "github.com/go-sql-driver/mysql"
 	lg "github.com/hiromaily/golibs/log"
 	tm "github.com/hiromaily/golibs/time"
 	u "github.com/hiromaily/golibs/utils"
