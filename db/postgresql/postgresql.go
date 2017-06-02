87cab64d68d0d46bb24bea172f4deda00bf6a423
hiromaily <hiromaily2@gmail.com>
2017-06-02 18:17:44 +0200

    formatted

--- db/postgresql/postgresql.go
+++ db/postgresql/postgresql.go
@@ -1,12 +1,12 @@
 package postgresql
 
 import (
-	_ "github.com/lib/pq"
 	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 )
 
-func New(dbname, user, pass string){
+func New(dbname, user, pass string) {
 	db, err := sql.Open("postgres", fmt.Sprintf("dbname=%s user=%s password=%s sslmode=disable", dbname, user, pass))
 	if err != nil {
 		panic(err)
