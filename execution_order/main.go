15275cc15d3aabf6c6657323e051919377d5378f
hiromaily <hiromaily2@gmail.com>
2018-02-10 21:47:51 +0100

    added new for execution order check

---
+++ execution_order/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hiromaily/golibs/execution_order/sub"
+)
+
+func init() {
+	fmt.Println("main.go: init()")
+}
+
+func main() {
+	fmt.Println("main.go: main()")
+
+	sub.Something()
+
+	//1. sub/sub.go: init()
+	//2. aaa.go: init()
+	//3. main.go: init()
+	//4. sub1.go: init()
+	//5. sub2.go: init()
+	//6. main.go: main()
+	//7. sub/sub.go: something()
+
+	//First,  except main, imported package's init() is called.
+	//Second, if main package includes several go files, these are executed by alphabetical order.
+}
---
+++ execution_order/sub/sub.go
@@ -0,0 +1,13 @@
+package sub
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("sub/sub.go: init()")
+}
+
+func Something() {
+	fmt.Println("sub/sub.go: something()")
+}
