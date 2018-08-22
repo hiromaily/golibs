aabd4122e185f54ec4ba117e16a8f72f7014db2d
hiromaily <hiromaily2@gmail.com>
2018-08-22 12:23:03 +0900

    added sample

---
+++ example/forloop/forloop_test.go
@@ -0,0 +1,53 @@
+// Package forloop_test is just sample
+package forloop_test
+
+import (
+	//lg "github.com/hiromaily/golibs/log"
+	//. "github.com/hiromaily/golibs/xml"
+	tu "github.com/hiromaily/golibs/testutil"
+	"os"
+	"testing"
+)
+
+//-----------------------------------------------------------------------------
+// Test Framework
+//-----------------------------------------------------------------------------
+// Initialize
+func init() {
+	tu.InitializeTest("[FORLOOP]")
+}
+
+func setup() {
+}
+
+func teardown() {
+}
+
+func TestMain(m *testing.M) {
+	setup()
+
+	code := m.Run()
+
+	teardown()
+
+	os.Exit(code)
+}
+
+//-----------------------------------------------------------------------------
+// function
+//-----------------------------------------------------------------------------
+func callSomthing() int {
+	for i := 0; i < 5; i++ {
+		if i == 3 {
+			return 10
+		}
+	}
+	return 5
+}
+
+//-----------------------------------------------------------------------------
+// Test
+//-----------------------------------------------------------------------------
+func TestForLoop(t *testing.T) {
+	t.Logf("result is %d", callSomthing())
+}
