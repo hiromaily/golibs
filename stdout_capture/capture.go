d0283440f22c5c5c910536aca7122f5a46631021
hiromaily <hiromaily2@gmail.com>
2019-01-28 20:02:01 +0900

    tweak

--- stdout_capture/capture.go
+++ stdout_capture/capture.go
@@ -21,3 +21,19 @@ func CaptureStdout(f func()) string {
 	io.Copy(&buf, r)
 	return buf.String()
 }
+
+// not thread safe
+func ContinuousCaptureStdout() string {
+	old := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	//f()
+
+	w.Close()
+	os.Stdout = old
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	return buf.String()
+}
--- stdout_capture/capture_test.go
+++ stdout_capture/capture_test.go
@@ -3,6 +3,7 @@ package stdout_capture_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	. "github.com/hiromaily/golibs/stdout_capture"
 )
@@ -13,4 +14,23 @@ func TestCaptureStdout(t *testing.T) {
 	}
 	captured := CaptureStdout(f)
 	fmt.Printf("captured: %s\n", captured)
+}
+
+// FIXME:このやり方では全部captureできるわけではない
+func TestCaptureStdout2(t *testing.T) {
+	go continuousRun()
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("test%d\n", i)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func continuousRun() {
+	for {
+		captured := ContinuousCaptureStdout()
+		if captured != "" {
+			fmt.Printf("captured: %s\n", captured)
+		}
+	}
 }
