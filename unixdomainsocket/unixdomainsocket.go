993e9546e1a4d764b33ed8ebe9e4bdefffd11a2c
hiromaily <hiromaily2@gmail.com>
2019-03-13 21:18:40 +0900

    added

---
+++ unixdomainsocket/unixdomainsocket.go
@@ -0,0 +1,90 @@
+package unixdomainsocket
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strconv"
+	"syscall"
+)
+
+// NewListner is to return net.listener object
+func NewListner() (net.Listener, string, error) {
+
+	// create sock file
+	sockFile := filepath.Join(os.TempDir(), "socketfile")
+	fmt.Printf("socket file: %s\n", sockFile)
+
+	// pid
+	pid := strconv.Itoa(os.Getpid())
+	fmt.Printf("pid: %s\n", pid)
+
+	// listen
+	listener, err := net.Listen("unix", sockFile)
+	if err != nil {
+		return nil, "", err
+	}
+
+	// change owner
+	if err := os.Chmod(sockFile, 0700); err != nil {
+		return nil, "", err
+	}
+
+	return listener, sockFile, nil
+}
+
+// Server is to receive request from client
+func Server(listener net.Listener) {
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return
+		}
+		go process(conn)
+	}
+}
+
+func process(conn net.Conn) {
+	for {
+		buf := make([]byte, 512)
+		nr, err := conn.Read(buf)
+		if err != nil {
+			break
+		}
+		data := buf[0:nr]
+		fmt.Printf("Recieved: %v", string(data))
+		_, err = conn.Write(data)
+		if err != nil {
+			log.Printf("error: %v\n", err)
+			break
+		}
+	}
+	conn.Close()
+}
+
+// WaitShutdown to wait shutdown by signal
+func WaitShutdown(listener net.Listener, close chan error) {
+	c := make(chan os.Signal, 1)
+	// wait Ctrl+C, kill -SIGTERM xxx
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		for {
+			s := <-c
+			switch s {
+			case syscall.SIGINT:
+				fmt.Println("[Signal] interrupt by control + C")
+			case syscall.SIGTERM:
+				fmt.Println("[Signal] force stop by kill command")
+			}
+			break
+		}
+		if err := listener.Close(); err != nil {
+			close <- err
+		}
+		close <- nil
+	}()
+}
---
+++ unixdomainsocket/unixdomainsocket_test.go
@@ -0,0 +1,33 @@
+package unixdomainsocket_test
+
+import (
+	"testing"
+
+	. "github.com/hiromaily/golibs/unixdomainsocket"
+)
+
+func TestServer(t *testing.T) {
+	//listner
+	listener, tempDir, err := NewListner()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// wait shutdown by signal
+	close := make(chan error)
+	WaitShutdown(listener, close)
+
+	// handle request from client
+	Server(listener)
+
+	//wait from shutdown
+	err = <-close
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// remove socket file
+	// if err := os.Remove(tempDir); err != nil {
+	// 	t.Fatal(err)
+	// }
+}
