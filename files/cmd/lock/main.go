0bd7ea696c895dcfe978487a5e29a60e002f3251
hiromaily <hiromaily2@gmail.com>
2019-03-14 21:03:29 +0900

    tweak

--- files/cmd/lock/main.go
+++ files/cmd/lock/main.go
@@ -5,19 +5,26 @@ import (
 	"time"
 
 	"github.com/hiromaily/golibs/files"
+	t "github.com/hiromaily/golibs/time"
 )
 
 func main() {
+	defer t.Track(time.Now(), "lock/main()")
+
 	l, err := files.NewFileLock("cmd/lock/main.go")
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("lock")
-	l.Lock()
+	if l.Lock() != nil {
+		panic(err)
+	}
 
 	time.Sleep(10 * time.Second)
 
-	l.Unlock()
+	if l.Unlock() != nil {
+		panic(err)
+	}
 	fmt.Println("unlock")
 }
