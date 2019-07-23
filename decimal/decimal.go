5dc41cacec93c64d89605106a8df7fedeb909ab4
hiromaily <hiromaily2@gmail.com>
2019-07-23 20:00:58 +0900

    added decimal package

--- decimal/decimal.go
+++ decimal/decimal.go
@@ -1,1 +1,82 @@
 package decimal
+
+import (
+	dc "github.com/ericlagergren/decimal"
+	"math/big"
+)
+
+// Notes: about scale
+// How do I interpret precision and scale of a number in a database?
+// https://stackoverflow.com/questions/2377174/how-do-i-interpret-precision-and-scale-of-a-number-in-a-database
+// ie 1234567.89 has a precision of 9
+// ie 123456.789 has a scale of 3
+
+type Decimal struct {
+	ctx dc.Context
+}
+
+// if 12345.12345 is expected,
+// New(10, 5, 0) //as precision 9,16,19,34
+func New(prec, maxScale, minScale int) *Decimal {
+	ctx := dc.Context{
+		Precision:     prec,
+		MaxScale:      maxScale,
+		MinScale:      minScale,
+		OperatingMode: dc.GDA,
+	}
+
+	return &Decimal{
+		ctx: ctx,
+	}
+}
+
+func (d *Decimal) String(v string) *dc.Big {
+	//TODO: error should be checked though
+	//b, _ := new(dc.Big).SetString(v)
+	b, _ := dc.WithContext(d.ctx).SetString(v)
+	return b
+}
+
+func (d *Decimal) Uint64(v uint64) *dc.Big {
+	//return new(dc.Big).SetUint64(v)
+	return dc.WithContext(d.ctx).SetUint64(v)
+}
+
+func (d *Decimal) Float64(v float64) *dc.Big {
+	//return new(dc.Big).SetFloat64(v)
+	return dc.WithContext(d.ctx).SetFloat64(v)
+}
+
+func (d *Decimal) BigFloat(v *big.Float) *dc.Big {
+	//return new(dc.Big).SetFloat(v)
+	return dc.WithContext(d.ctx).SetFloat(v)
+}
+
+func (d *Decimal) Add(x0, y0 *dc.Big) *dc.Big {
+	x := dc.WithContext(d.ctx).Set(x0)
+	y := dc.WithContext(d.ctx).Set(y0)
+	dc.WithContext(d.ctx).Int64()
+
+	return x.Add(x, y)
+}
+
+func (d *Decimal) Sub(x0, y0 *dc.Big) *dc.Big {
+	x := dc.WithContext(d.ctx).Set(x0)
+	y := dc.WithContext(d.ctx).Set(y0)
+
+	return x.Sub(x, y)
+}
+
+func (d *Decimal) Mul(x0, y0 *dc.Big) *dc.Big {
+	x := dc.WithContext(d.ctx).Set(x0)
+	y := dc.WithContext(d.ctx).Set(y0)
+
+	return x.Mul(x, y)
+}
+
+func (d *Decimal) Div(x0, y0 *dc.Big) *dc.Big {
+	x := dc.WithContext(d.ctx).Set(x0)
+	y := dc.WithContext(d.ctx).Set(y0)
+
+	return x.Quo(x, y)
+}
--- decimal/decimal_test.go
+++ decimal/decimal_test.go
@@ -1,55 +1,79 @@
 package decimal_test
 
 import (
-	"fmt"
 	"testing"
 
-	dc "github.com/ericlagergren/decimal"
+	"github.com/hiromaily/golibs/decimal"
 )
 
 //https://github.com/ericlagergren/decimal
 //https://github.com/ericlagergren/decimal/blob/master/example_decimal_test.go
 
-func TestDecimalString(t *testing.T) {
+func TestString(t *testing.T) {
+	// if 12345.12345 is expected,
+	// New(10, 5, 0) //as precision 9,16,19,34
+	deci := decimal.New(10, 5, 0)
+
 	var tests = []struct {
 		strDecimal  string
-		format      string
+		expected    string
 		description string
 	}{
-		{"123456789.12345678901234567890", "%s", ""},
-		{"123456789.12345678901234567890", "%.1f", "decimal point but no exponent, e.g. 123.456"},
-		{"123456789.12345678901234567890", "%10.f", ""},
-		{"123456789.12345678901234567890", "%10.10f", ""},
-		{"123456789.12345678901234567890", "%10.20f", ""},
-		//{"123456789.123456789",  "%.3g", "%e for large exponents, %f otherwise. Precision is discussed below."},
-		//{"123456789.123456789",  "%6.4g", ""},
+		{"12345.12345", "12345.12345", ""},
+		{"123456.12345", "12345.12345", ""},
+		{"12345.123456", "12345.12345", ""},
+		{"123456.123456", "12345.12345", ""},
 	}
 
 	for _, val := range tests {
-		d, _ := new(dc.Big).SetString(val.strDecimal)
-		fmt.Printf(val.format+"\n", d)
+		ret := deci.String(val.strDecimal)
+		if ret.String() != val.expected {
+			t.Errorf("want: %s, but result: %s", val.expected, ret)
+		}
+		//fmt.Printf("string:%s, dc.Big:%s\n", val.strDecimal, deci.String(val.strDecimal))
 	}
 }
 
-func TestConvert(t *testing.T) {
-	// 1 wei is 1,000,000,000,000,000,000 wei
-
-	var tests = []struct {
-		amount   string
-		expected string
-	}{
-		{"1", "1000000000000000000"},
-		{"0.35", "350000000000000000"},
-		{"1.35", "1350000000000000000"},
-		{"21.35", "21350000000000000000"},
-		{"21.356", "21356000000000000000"},
-		{"321.35", "321350000000000000000"},     //decimalでは、この桁からうまく返せない。。。
-		{"99999.35", "99999350000000000000000"}, //
-	}
-
-	for _, val := range tests {
-		d, _ := new(dc.Big).SetString(val.amount)
-		fmt.Printf("%30f\n", d.Quantize(-30))
-	}
-
-}
+//func TestDecimalString(t *testing.T) {
+//	var tests = []struct {
+//		strDecimal  string
+//		format      string
+//		description string
+//	}{
+//		{"123456789.12345678901234567890", "%s", ""},
+//		{"123456789.12345678901234567890", "%.1f", "decimal point but no exponent, e.g. 123.456"},
+//		{"123456789.12345678901234567890", "%10.f", ""},
+//		{"123456789.12345678901234567890", "%10.10f", ""},
+//		{"123456789.12345678901234567890", "%10.20f", ""},
+//		//{"123456789.123456789",  "%.3g", "%e for large exponents, %f otherwise. Precision is discussed below."},
+//		//{"123456789.123456789",  "%6.4g", ""},
+//	}
+//
+//	for _, val := range tests {
+//		d, _ := new(dc.Big).SetString(val.strDecimal)
+//		fmt.Printf(val.format+"\n", d)
+//	}
+//}
+//
+//func TestConvert(t *testing.T) {
+//	// 1 wei is 1,000,000,000,000,000,000 wei
+//
+//	var tests = []struct {
+//		amount   string
+//		expected string
+//	}{
+//		{"1", "1000000000000000000"},
+//		{"0.35", "350000000000000000"},
+//		{"1.35", "1350000000000000000"},
+//		{"21.35", "21350000000000000000"},
+//		{"21.356", "21356000000000000000"},
+//		{"321.35", "321350000000000000000"},     //decimalでは、この桁からうまく返せない。。。
+//		{"99999.35", "99999350000000000000000"}, //
+//	}
+//
+//	for _, val := range tests {
+//		d, _ := new(dc.Big).SetString(val.amount)
+//		fmt.Printf("%30f\n", d.Quantize(-30))
+//	}
+//
+//}
