919a8e6551ab8232e6fb9e40a1f61d036116c83b
hiromaily <hiromaily2@gmail.com>
2019-10-06 18:47:16 +0900

    update

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"context"
 	"flag"
-	"google.golang.org/grpc/codes"
 	"io"
 	"log"
 	"net"
 	"time"
 
-	samplepb "github.com/hiromaily/golibs/protobuf/pb/sample"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	samplepb "github.com/hiromaily/golibs/protobuf/pb/sample"
 )
 
 const (
@@ -23,6 +24,33 @@ var (
 	name     = flag.String("name", "", "name")
 	question = flag.Int64("q", 0, "question code")
 )
+
+var clients = []*samplepb.Client{
+	{
+		QuestionCode: 1,
+		Name:         "Mike",
+	},
+	{
+		QuestionCode: 5,
+		Name:         "Salon",
+	},
+	{
+		QuestionCode: 10,
+		Name:         "Quen",
+	},
+	{
+		QuestionCode: 18,
+		Name:         "Lin",
+	},
+	{
+		QuestionCode: 29,
+		Name:         "Michael",
+	},
+	{
+		QuestionCode: 50,
+		Name:         "Rocky",
+	},
+}
 
 func init() {
 	flag.Parse()
@@ -37,7 +65,6 @@ func validate() {
 }
 
 func main() {
-	validate()
 
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
@@ -54,6 +81,7 @@ func main() {
 
 	switch *mode {
 	case 1:
+		validate()
 		//Unary
 		doUnary(ctx, cli)
 	case 2:
@@ -62,6 +90,8 @@ func main() {
 	case 3:
 		//ClientStreaming
 		doClientStreaming(ctx, cli)
+	case 4:
+		doBidirectionalStreaming(ctx, cli)
 	default:
 		log.Fatal("-mode is out of range")
 	}
@@ -73,37 +103,50 @@ func doUnary(ctx context.Context, cli samplepb.SampleServiceClient) {
 		Name:         *name,
 	})
 	if err != nil {
-		log.Fatalf("fail to call cli.UnaryAsk(): %v", err)
+		if respErr, ok := status.FromError(err); ok {
+			switch respErr.Code() {
+			case codes.InvalidArgument:
+				log.Println("parameter is invalid")
+			case codes.DeadlineExceeded:
+				log.Println("timeout")
+			default:
+				log.Printf("something grpc error: %d, %s", respErr.Code(), respErr.Message())
+			}
+		} else {
+			log.Fatalf("fail to call cli.UnaryAsk(): %v", err)
+		}
+		return
 	}
 	log.Printf("Response: [code] %d, [answer] %s", res.Code, res.Answer)
 }
 
 func doServerStreaming(ctx context.Context, cli samplepb.SampleServiceClient) {
 	req := &samplepb.ManyClients{
-		Clients: []*samplepb.Client{
-			{
-				QuestionCode: *question,
-				Name:         *name,
-			},
-			{
-				QuestionCode: *question + 1,
-				Name:         *name + "2",
-			},
-			{
-				QuestionCode: *question + 2,
-				Name:         *name + "3",
-			},
-		},
+		Clients: clients,
 	}
+	log.Println("[doServerStreaming] send from client")
 	resStream, err := cli.ServerStreamingRespondManytimes(ctx, req)
 	if err != nil {
 		log.Fatalf("fail to call cli.ServerStreamingRespondManytimes(): %v", err)
 	}
 	for {
 		res, err := resStream.Recv()
-		if isError(err) {
-			//rpc error: code = DeadlineExceeded desc = context deadline exceeded
-			log.Fatalf("fail to receive response from Recv(): %v", err)
+		if err == io.EOF {
+			log.Println("server is closed")
+			break
+		} else if err != nil {
+			if respErr, ok := status.FromError(err); ok {
+				switch respErr.Code() {
+				case codes.InvalidArgument:
+					log.Println("parameter is invalid")
+				case codes.DeadlineExceeded:
+					log.Println("timeout")
+				default:
+					log.Printf("something grpc error: %d, %s", respErr.Code(), respErr.Message())
+				}
+			} else {
+				log.Fatalf("fail to receive response from Recv(): %v", err)
+			}
 			break
 		}
 		log.Printf("Response: [code] %d, [answer] %s", res.Code, res.Answer)
@@ -116,41 +159,91 @@ func doClientStreaming(ctx context.Context, cli samplepb.SampleServiceClient) {
 		log.Fatalf("fail to call cli.ClientStreamingAskManytimes(): %v", err)
 	}
 
-	//req := &samplepb.Client{
-	//	QuestionCode: *question,
-	//	Name:         *name,
-	//}
-
 	reqs := &samplepb.ManyClients{
-		Clients: []*samplepb.Client{
-			{
-				QuestionCode: *question,
-				Name:         *name,
-			},
-			{
-				QuestionCode: *question + 1,
-				Name:         *name + "2",
-			},
-			{
-				QuestionCode: *question + 2,
-				Name:         *name + "3",
-			},
-		},
+		Clients: clients,
 	}
 
 	for _, req := range reqs.Clients {
+		log.Println("send from client")
 		stream.Send(req)
-		time.Sleep(200 * time.Millisecond)
+		time.Sleep(100 * time.Millisecond)
 	}
 
+	log.Println("receive from server")
 	res, err := stream.CloseAndRecv()
 	if err != nil {
-		log.Fatalf("fail to receive response from CloseAndRecv(): %v", err)
+		if respErr, ok := status.FromError(err); ok {
+			switch respErr.Code() {
+			case codes.InvalidArgument:
+				log.Println("parameter is invalid")
+			case codes.DeadlineExceeded:
+				log.Println("timeout")
+			default:
+				log.Printf("something grpc error: %d, %s", respErr.Code(), respErr.Message())
+			}
+		} else {
+			log.Fatalf("fail to receive response from Recv(): %v", err)
+		}
+		return
 	}
 
-	for idx, answer := range res.Answers {
-		log.Printf("Response: [%d][code] %d, [answer] %s", idx, answer.Code, answer.Answer)
+	for _, answer := range res.Answers {
+		log.Printf("Response: [code] %d, [answer] %s", answer.Code, answer.Answer)
 	}
+}
+
+func doBidirectionalStreaming(ctx context.Context, cli samplepb.SampleServiceClient) {
+	stream, err := cli.BidirectionalStreaming(ctx)
+	if err != nil {
+		log.Fatalf("fail to call cli.BidirectionalStreaming(): %v", err)
+	}
+
+	waitc := make(chan struct{})
+
+	reqs := &samplepb.ManyClients{
+		Clients: clients,
+	}
+
+	//send
+	go func() {
+		for _, client := range reqs.Clients {
+			log.Println("send from client")
+			stream.Send(client)
+			time.Sleep(200 * time.Millisecond)
+		}
+		if err := stream.CloseSend(); err != nil {
+			log.Fatalf("fail to call CloseSend(): %v", err)
+		}
+	}()
+
+	//receive
+	go func() {
+		for {
+			log.Println("receive from server")
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			} else if err != nil {
+				if respErr, ok := status.FromError(err); ok {
+					switch respErr.Code() {
+					case codes.InvalidArgument:
+						log.Println("parameter is invalid")
+					case codes.DeadlineExceeded:
+						log.Println("timeout")
+					default:
+						log.Printf("something grpc error: %d, %s", respErr.Code(), respErr.Message())
+					}
+				} else {
+					log.Fatalf("fail to receive response from Recv(): %v", err)
+				}
+				break
+			}
+			log.Printf("Response: [code] %d, [answer] %s", res.Code, res.Answer)
+		}
+		close(waitc)
+	}()
+
+	<-waitc
 }
 
 func isError(err error) bool {
@@ -174,10 +267,12 @@ func isError(err error) bool {
 	case codes.DeadlineExceeded:
 		log.Println("timeout by grpc.DeadlineExceeded")
 		return true
+	case codes.InvalidArgument:
+		log.Println("invalid error")
+		return true
 	case
 		codes.Canceled,
 		codes.Unknown,
-		codes.InvalidArgument,
 		codes.NotFound,
 		codes.AlreadyExists,
 		codes.PermissionDenied,
--- grpc/server/main.go
+++ grpc/server/main.go
@@ -8,9 +8,11 @@ import (
 	"net"
 	"time"
 
-	samplepb "github.com/hiromaily/golibs/protobuf/pb/sample"
-	"github.com/pkg/errors"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	samplepb "github.com/hiromaily/golibs/protobuf/pb/sample"
 )
 
 const (
@@ -24,29 +26,40 @@ type server struct{}
 func (s *server) UnaryAsk(ctx context.Context, in *samplepb.Client) (*samplepb.Answer, error) {
 	log.Printf("[UnaryAsk] Received: name: %s, question code: %d", in.Name, in.QuestionCode)
 
+	//validate
+	if in.QuestionCode >= 100 {
+		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintf("invalid question_code: %v", in.QuestionCode))
+	}
+	if in.QuestionCode == 10 {
+		//for deadline test
+		time.Sleep(2000 * time.Millisecond)
+	}
+
 	return &samplepb.Answer{
 		Code:   200,
-		Answer: fmt.Sprintf("Hi %s, ", in.Name),
+		Answer: fmt.Sprintf("Hi %s, your question_id is %d", in.Name, in.QuestionCode),
 	}, nil
 }
+
 func (s *server) ServerStreamingRespondManytimes(req *samplepb.ManyClients, stream samplepb.SampleService_ServerStreamingRespondManytimesServer) error {
 	log.Println("[ServerStreamingRespondManytimes]")
 
+	//validate
 	if len(req.GetClients()) == 0 {
-		//TODO: error something
-		return errors.New("clients has nothing")
+		return status.Errorf(codes.InvalidArgument, "client has nothing")
 	}
 
 	// create multiple answer
 	for i, client := range req.GetClients() {
 		//log.Printf("[ServerStreamingAskManytimes] Received: name: %s, question code: %d", client.Name, client.QuestionCode)
-		log.Printf("[ServerStreamingAskManytimes] Received: name: %s, question code: %d", client.GetName(), client.GetQuestionCode())
+		log.Printf("Received: name: %s, question code: %d", client.GetName(), client.GetQuestionCode())
 
 		answer := &samplepb.Answer{
 			Code:   200,
 			Answer: fmt.Sprintf("[%d]Hi %s", i, client.GetName()),
 		}
 		// send
+		log.Println("send to client")
 		if err := stream.Send(answer); err != nil {
 			return err
 		}
@@ -65,17 +78,46 @@ func (s *server) ClientStreamingAskManytimes(stream samplepb.SampleService_Clien
 	for {
 		req, err := stream.Recv()
 		if err == io.EOF {
+			log.Println("server is closed after sending")
 			return stream.SendAndClose(&samplepb.ManyAnswers{Answers: answers})
 		} else if err != nil {
-			log.Println("err: ", err)
+			log.Println("fail to call Recv(): ", err)
 			return err
 		}
 
 		answer := &samplepb.Answer{
 			Code:   200,
-			Answer: fmt.Sprintf("[%d]Hi %s", idx, req.GetName()),
+			Answer: fmt.Sprintf("Hi %s, your question_id is %d", req.GetName(), req.GetQuestionCode()),
 		}
 		answers = append(answers, answer)
+
+		idx++
+	}
+}
+
+func (s *server) BidirectionalStreaming(stream samplepb.SampleService_BidirectionalStreamingServer) error {
+	log.Println("[BidirectionalStreaming]")
+
+	var idx uint64
+	for {
+		//receive
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		} else if err != nil {
+			log.Println("fail to call Recv(): ", err)
+			return err
+		}
+
+		//send
+		err = stream.Send(&samplepb.Answer{
+			Code:   200,
+			Answer: fmt.Sprintf("[%d]Hi %s", idx, req.GetName()),
+		})
+		if err != nil {
+			log.Println("fail to call Send(): ", err)
+			return err
+		}
 
 		idx++
 	}
